efc789f5362d1623d1f4d3aa31950713aa94341d
Andy Simota <andyiulian.simota@gmail.com>
2025-01-23 14:55:00 +0100

    added cors policy and implemented notFound page

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 const express = require("express");
 // creo istanza
 const app = express();
-
 // set port
 const port = process?.env.PORT || 3000;
 
 //altri import
 const moviesRouter = require("./routers/moviesRouter");
+const notFound = require("./middlewares/notFound");
+
+//npm i cors
+const cors = require("cors");
+//uso corse prima di tutte le rotte
+app.use(cors());
 
 app.use(express.static("public"));
 
@@ -16,6 +21,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/movies", moviesRouter);
+
+//metto in ascolto un errore 404
+app.use(notFound);
 
 //metto il server in ascolto
 app.listen(port, () => {
---
+++ middlewares/notFound.js
@@ -0,0 +1,6 @@
+function notFound(req, res, next) {
+  res.status(404);
+  res.json({ error: "Not Found", message: "Rotta non trovata" });
+}
+
+module.exports = notFound;
