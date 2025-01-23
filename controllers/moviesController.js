492517159b60696f4f50b645afce6d875fc09ae5
Andy Simota <andyiulian.simota@gmail.com>
2025-01-23 16:12:26 +0100

    aggiunta show piu review annessi

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,5 +11,34 @@ const index = (req, res) => {
     res.json(results);
   });
 };
+const show = (req, res) => {
+  // Recuperiamo l'ID dall'URL
+  const id = req.params.id;
+  // Query per ottenere i dettagli del film
+  const moviesSql = `SELECT * FROM movies 
+  WHERE id = ?`;
+  // Query per ottenere le recensioni del film
+  const reviewsSql = `SELECT * FROM reviews 
+        WHERE movie_id = ?`;
+
+  // Eseguiamo la query per il film
+  connection.query(moviesSql, [id], (err, movieResults) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (movieResults.length === 0)
+      return res.status(404).json({ error: "Film non trovato" });
+    // Recuperiamo il film dal risultato della prima query
+    const movie = movieResults[0];
+
+    // Eseguiamo  query per le recensioni
+    connection.query(reviewsSql, [id], (err, reviewsResults) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+      // Aggiungiamo le recensioni al film
+      movie.reviews = reviewsResults;
+      // Restituiamo il film con le recensioni
+      res.json(movie);
+    });
+  });
+};
+
 // esporto la funzione nelle rotte
-module.exports = { index };
+module.exports = { index, show };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -3,10 +3,11 @@ const express = require("express");
 // creo un router
 const router = express.Router();
 // importo il controller
-const { index } = require("../controllers/moviesController");
+const { index, show } = require("../controllers/moviesController");
 
 // definisco le rotte
 router.get("/", index);
+router.get("/:id", show);
 
 // esporto le rotte
 module.exports = router;
